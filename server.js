bc6de03617d4a067b048da32995be0e030afb391
Naeltonf <naeltonfdasilva@gmail.com>
2024-07-02 22:16:19 -0300

    A1-08

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ import http from "http"; //biblioteca nativa do node
 
 const PORT = 3000;
 
+const rotas = {
+    "/": "msg",
+    "/livros": "rota 1",
+    "/autores": "rota 2"
+};
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("msg")
+    res.end(rotas[req.url])
 });
 
 server.listen(PORT, () => {
